56178da709759089a7f65d30f9cf2d8bddbe117a
daanish04 <108368229+daanish04@users.noreply.github.com>
2025-05-30 12:59:42 +0530

    fix: checkUser function

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import HeaderActions from "./headerActions";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <div className="sticky top-0 w-full backdrop-blur-lg bg-white/30 border border-gray-200 z-20 dark:bg-zinc-900/30 dark:border-zinc-800">
       <nav className="flex justify-between items-center px-4 py-2">
